2c3549ba4e12f3d1e7531fd99c7cc17db544c00a
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2025-01-06 15:18:03 +0100

    Ajout des adresses DIS locales et distantes à utiliser.

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 
 def load_config_from_env():
+    own_dis_site = os.getenv("OWN_DIS_SITE", 29)
+    own_dis_application = os.getenv("OWN_DIS_APPLICATION", 1)
+
+    remote_dis_site = os.getenv("REMOTE_DIS_SITE", 1)
+    remote_dis_application = os.getenv("REMOTE_DIS_APPLICATION", 42)
+
     # Receiver Configuration
     udp_receiver_ip = os.getenv("DIS_RECEIVER_IP", "0.0.0.0")
     udp_receiver_port = int(os.getenv("DIS_RECEIVER_PORT", "3000"))
     udp_receiver_mode = os.getenv("DIS_RECEIVER_MODE", "multicast")
 
     # Emitter Configuration
-    udp_emitter_ip = os.getenv("DIS_EMITTER_IP", "255.255.255.255")
+    udp_emitter_ip = os.getenv("DIS_EMITTER_IP", "127.0.0.255")
     udp_emitter_port = int(os.getenv("DIS_EMITTER_PORT", "4000"))
     udp_emitter_mode = os.getenv("DIS_EMITTER_MODE", "broadcast")
 
@@ -22,6 +28,10 @@ def load_config_from_env():
     http_token_poller = os.getenv("HTTP_BEARER_TOKEN_POLLER", default_token)
 
     return {
+        "own_dis_site" : own_dis_site,
+        "own_dis_application" : own_dis_application,
+        "remote_dis_site" : remote_dis_site,
+        "remote_dis_application" : remote_dis_application,
         "receiver": {"ip": udp_receiver_ip, "port": udp_receiver_port, "mode": udp_receiver_mode},
         "emitter": {"ip": udp_emitter_ip, "port": udp_emitter_port, "mode": udp_emitter_mode},
         "http_receiver": http_endpoint_receiver,
