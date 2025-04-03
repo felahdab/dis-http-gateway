6a74269a89c454061d6e087ceca9cbe8535a84f9
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2025-04-03 15:26:59 +0200

    Isolement de la methode pdu_to_dict pour pouvoir l'utiliser plus globalement.

--- distools/dis_receiver.py
+++ distools/dis_receiver.py
@@ -4,31 +4,13 @@ from twisted.internet.protocol import DatagramProtocol
 
 from .pdus.pdu_extension import extend_pdu_factory
 from .pdus.transfer_ownership_pdu import TransferOwnershipPdu
+from .pdus.tools import pdu_to_dict
 
 # Extend the PduFactory to include custom PDUs
 extend_pdu_factory()
 
 from opendis.dis7 import EntityStatePdu
 from opendis import PduFactory
-
-def pdu_to_dict(pdu_object):
-    """
-    Transforme un objet PDU en dictionnaire Python.
-    Parcourt récursivement les attributs de l'objet.
-    """
-    result = {}
-    for attribute in dir(pdu_object):
-        if not attribute.startswith("_") and not callable(getattr(pdu_object, attribute)):
-            value = getattr(pdu_object, attribute)
-            if isinstance(value, list):
-                # Si c'est une liste, applique la conversion à chaque élément
-                result[attribute] = [pdu_to_dict(item) if hasattr(item, "__dict__") else item for item in value]
-            elif hasattr(value, "__dict__"):
-                # Si c'est un objet complexe, appelle récursivement
-                result[attribute] = pdu_to_dict(value)
-            else:
-                result[attribute] = value
-    return result
 
 class DISReceiver(DatagramProtocol):
     def __init__(self, poster, broadcast):
---
+++ distools/pdus/tools.py
@@ -0,0 +1,18 @@
+def pdu_to_dict(pdu_object):
+    """
+    Transforme un objet PDU en dictionnaire Python.
+    Parcourt récursivement les attributs de l'objet.
+    """
+    result = {}
+    for attribute in dir(pdu_object):
+        if not attribute.startswith("_") and not callable(getattr(pdu_object, attribute)):
+            value = getattr(pdu_object, attribute)
+            if isinstance(value, list):
+                # Si c'est une liste, applique la conversion à chaque élément
+                result[attribute] = [pdu_to_dict(item) if hasattr(item, "__dict__") else item for item in value]
+            elif hasattr(value, "__dict__"):
+                # Si c'est un objet complexe, appelle récursivement
+                result[attribute] = pdu_to_dict(value)
+            else:
+                result[attribute] = value
+    return result
