da3ca870f48c21c9ef94f08395be58402a1ed8b9
Florian El-Ahdab <florian.el-ahdab@m4x.org>
2025-04-06 00:09:44 +0200

    Introduction paramètre ignore_certificate dans le poster HTTP

--- httptools/http_poster.py
+++ httptools/http_poster.py
@@ -1,16 +1,28 @@
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 from twisted.internet import reactor, task
+from twisted.internet.ssl import ClientContextFactory
 import json
 from .memory_body_producer import MemoryBodyProducer
 
+class WebClientContextFactory(ClientContextFactory):
+    def getContext(self, hostname, port):
+        return ClientContextFactory.getContext(self)
+    
+
 class HttpPoster:
-    def __init__(self, http_endpoint, http_token):
+    def __init__(self, http_endpoint, http_token, ignore_certificate):
         self.http_endpoint = http_endpoint
         self.http_token = http_token
+        self.ignore_certificate = ignore_certificate
+        if ignore_certificate:
+            contextFactory = WebClientContextFactory()
+            self.agent = Agent(reactor, contextFactory)
+        else:
+            self.agent = Agent(reactor)
 
     def post_to_api(self, jsondumpable):
-        agent = Agent(reactor)
+        agent = self.agent
         headers = Headers({
             "Content-Type": ["application/json"],
             "Authorization": [f"Bearer {self.http_token}"]
